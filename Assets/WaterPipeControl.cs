36ed0aadbf26abe61c3266b2cbbf8371cf8551fe
XiaoMac <1@xiao.studio>
2022-03-03 08:00:27 -0500

    IoC Prototype WIP1
    in progress

--- Assets/WaterPipeControl.cs
+++ Assets/WaterPipeControl.cs
@@ -14,7 +14,7 @@ public class WaterPipeControl : MonoBehaviour {
   public GameObject secondDoor;
   public GameObject parentApple;
   public AppleRotate appleRotate;
-  Sequence animSquence = DOTween.Sequence();
+
 
   private int clickCount = 0;
   private bool isRotate = false;
@@ -31,6 +31,7 @@ public class WaterPipeControl : MonoBehaviour {
   public void OnClick() {
     if (isRotate) return;
     clickCount++;
+    Debug.Log(clickCount);
     isRotate = true;
     cubesParent.DORotate(new Vector3(0, 0, clickCount * 90), 1).OnComplete(() => {
       if (clickCount == 3) {
@@ -42,6 +43,7 @@ public class WaterPipeControl : MonoBehaviour {
       }
       else if (clickCount == 5)
       {
+        Debug.Log(clickCount);
         bottomSphere.GetComponent<Renderer>().material.DOColor(new Color(0.86f, 0.2f, 0.73f), 1).OnComplete(() => {
           tree.DOScale(1.5f, 1);
           tree.transform.DOLocalMove(biggerTreePos.localPosition, 1).OnComplete(() => {
@@ -76,9 +78,5 @@ public class WaterPipeControl : MonoBehaviour {
     });
     //leftCube.GetComponent<Renderer>().material.DOColor(new Color(0.86f, 0.2f, 0.73f), 1);
   }
-  public void playani() {
-    animSquence.Append(firstDoor.transform.DOLocalMove(new Vector3(0, 0, -0.5f), 2f));
-    animSquence.Append(parentApple.transform.DORotate(new Vector3(0, 90f, 0), 2f));
-    animSquence.Append(secondDoor.transform.DOLocalMove(new Vector3(0.5f, 0, -0.5f), 2f));
-  }
+ 
 }
