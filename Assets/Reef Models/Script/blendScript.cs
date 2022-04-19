c47bfa939870f2f2300b2ae3eb88accef71cab9c
Xiao <1@xiao.studio>
2022-04-19 16:41:27 -0400

    Material swap v0.1

--- Assets/Reef Models/Script/blendScript.cs
+++ Assets/Reef Models/Script/blendScript.cs
@@ -1,50 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class blendScript : MonoBehaviour
 {
-    // Blends between two materials
-
-    private Material material1;
+  // Blends between two materials
+    public GameObject tree;
+    public Material material1;
     public Material material2;
-    float duration = 1.0f;
+    float duration = 2f;
     public Renderer rend;
 
-    void Start()
-    {
-        material1 = GetComponent<Renderer>().material;
-        // At start, use the first material
-            Debug.Log(material1);
+    void Start() {
+    rend.material = tree.GetComponent<MeshRenderer>().materials[0];
+  }
+
+    void Update() {
+    //material1.DOFade(0, 2);
+    if (Input.GetKeyDown(KeyCode.C)) {
+      material1 = tree.GetComponent<MeshRenderer>().materials[0];
+      print(material1);
+      material2 = tree.GetComponent<MeshRenderer>().materials[1];
+      rend.material.Lerp(material1, material2, 1);
+      material1.DOColor(new Color32(143, 0, 254, 1), 1).OnComplete(() => {
+        rend.material = tree.GetComponent<MeshRenderer>().materials[1];
+      });
+      //StartCoroutine(ChangeMaterial());
     }
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.C)) {
-      //ChangeMaterial();
-        GetComponent<Renderer>().material = material2;
-
-    }
-
-        if(rend.material == material1){
-            Debug.Log("im in material1");
-        } else if(rend.material == material2){
-            Debug.Log("im in material2");
-        }
-        // ping-pong between the materials over the duration
-
-    }
+  }
 
     public IEnumerator ChangeMaterial() {
-    float a = 0;
-  while (a <= 1) {
-    Debug.Log("material changing");
-    yield return new WaitForEndOfFrame();
-        Material matA = material1;
-        Material matB = material2;
-        //matA = a;
-        matA = matB;
-        a += Time.deltaTime / 3;
-  }
-  }
+      float a = 0;
+      Debug.Log(a);
+      while (a <= 1) {
+      Debug.Log("material2 showing");
+      yield return new WaitForEndOfFrame();
+      Material material2 = tree.GetComponent<Renderer>().materials[0];
+      Color c = material2.color;
+      c.a = a;
+      material2.color = c;
+      a += Time.deltaTime / 3;
+    }
+    }
 }
+
