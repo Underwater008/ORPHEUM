6940f1859f8e17e3837bc7e29527f13524a83df2
Kevin Long <yl6260@nyu.edu>
2022-05-09 11:27:26 -0400

    Final Adjustments

--- Assets/Scripts/EndingGameEvt.cs
+++ Assets/Scripts/EndingGameEvt.cs
@@ -38,8 +38,8 @@ public class EndingGameEvt : MonoBehaviour
 
           endingDissovle.gameObject.SetActive(true);
           AppleLowPoly.transform.SetParent(puzzleSequenceControl.transform.parent);
-          //AppleLowPoly.transform.DOLocalMoveY(15f,1f).OnComplete(()=>{
-           puzzleSequenceControl.gameObject.SetActive(false);
+           //AppleLowPoly.transform.DOLocalMoveY(15f,1f).OnComplete(()=>{
+           puzzleSequenceControl.gameObject.transform.DOMove(new Vector3(0, 0, 0), 0);
             AppleLowPoly.transform.DORotate(new Vector3(0,360,0),5f).SetRelative(true).SetEase(Ease.Linear).SetLoops(-1,LoopType.Restart);
 
             UIManager.creditsPanel.gameObject.SetActive(true);
